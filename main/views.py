8c9d7e72bedf20b5e73cdc779946c82189164eb2
harkingberah <akingberaa115@gmail.com>
2023-03-29 11:00:33 -0700

    deploy my project

--- main/views.py
+++ main/views.py
@@ -285,7 +285,7 @@ def payment(request):
         profile = Customer.objects.get(user__username=request.user.username)
         api_key = 'sk_test_db4d6edb1fdca6fdcf896e2264d97d18eb6db3b6' #secret key from paystack
         curl = 'https://api.paystack.co/transaction/initialize' #paystack call url
-        cburl = 'http://13.50.231.131/callback' #payment confirmation page
+        cburl = 'http://13.53.171.174/callback' #payment confirmation page
         ref = str(uuid.uuid4()) # reference id required by paystack as an additional reference number
         order_no = profile.id
         total = float(request.POST['total']) * 100 #total amount to be charged from customer card
